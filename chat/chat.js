0de993c1f8206c84625f52284de0f7faa7bace42
Justin Peck <jmp51608@creighton.edu>
2015-11-30 10:43:08 -0600

    add functioning chat

--- chat/chat.js
+++ chat/chat.js
@@ -14,21 +14,58 @@ angular.module('creightonDir.chat', [
       }
     });
   })
-  .controller('ChatCtrl', function HomeController(socket, store, jwtHelper, $rootScope) {
+  .controller('ChatCtrl', function HomeController(socket, store, jwtHelper, $rootScope, $timeout) {
+    $rootScope.showNavBar = true;
     var chat = this;
     chat.messages = [];
-    $rootScope.showNavBar = true;
 
-    var token = store.get('jwt');
-    var jwt = jwtHelper.decodeToken(token);
+    var token = store.get('jwt'),
+      jwt = jwtHelper.decodeToken(token);
+    var chatArrays = {
+      global: [],
+      freshman: [],
+      sophomore: [],
+      junior: [],
+      senior: []
+    };
+    chat.room = 'global';
+    chat.messages = chatArrays.global;
+
+
+    chat.join = function(index) {
+      var rooms = ['global', 'freshman', 'sophomore', 'junior', 'senior'];
+      if (chat.room !== rooms[index]) {
+        socket.emit('switch', {
+          oldRoom: 'global',
+          newRoom: rooms[index]
+        });
+        chat.room = rooms[index];
+        chat.messages = chatArrays[rooms[index]];
+      }
+    }
 
     chat.send = function() {
-      socket.emit('sentMsg', {name: jwt.name, message: chat.input});
+      socket.emit('sentMsg', {
+        name: jwt.name,
+        message: chat.input,
+        room: chat.room
+      });
       chat.input = '';
+      updateScroll();
     }
 
     socket.on('message', function(sent) {
-        chat.messages.push({name: sent.name, message: sent.message});
+      chat.messages.push({
+        name: sent.name,
+        message: sent.message
+      });
     });
+
+    function updateScroll() {
+      $timeout(function() {
+        var elm = document.getElementById('chatRoom');
+        elm.scrollTop = elm.scrollHeight;
+      }, 20, false);
+    }
 
   });
