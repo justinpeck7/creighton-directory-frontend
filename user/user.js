4beabff0149ab865f0562f05a904d3d18a801de3
Justin Peck <justin@mbp.ddns.creighton.edu>
2015-12-02 16:00:12 -0600

    add form to send image

--- user/user.js
+++ user/user.js
@@ -13,11 +13,30 @@ angular.module('creightonDir.user', [
       }
     });
   })
-  .controller('UserCtrl', function HomeController($rootScope, $stateParams, $resource) {
+  .controller('UserCtrl', function HomeController($rootScope, $stateParams, $resource, store, jwtHelper) {
     $rootScope.showNavBar = true;
     var user = this,
-      FindOne = $resource('http://localhost:3001/user/auth/findOne');
+      FindOne = $resource('http://localhost:3001/user/auth/findOne'),
+      token = store.get('jwt'),
+      jwt = jwtHelper.decodeToken(token),
+      netId = jwt.netId;
     user.loading = true;
+
+       function formDataObject (data) {
+            var fd = new FormData();
+            angular.forEach(data, function(value, key) {
+                fd.append(key, value);
+            });
+            return fd;
+        }
+
+    var Upload = $resource('http://localhost:3001/upload/auth/picture', {}, {
+      save: {
+        method: 'POST',
+        transformRequest: formDataObject,
+        headers: {'Content-Type':undefined, enctype:'multipart/form-data'}
+      }
+    })
 
     /*Find a single user and return results*/
     FindOne.get({
@@ -27,4 +46,14 @@ angular.module('creightonDir.user', [
       user.info.groups = user.info.groups.join(', ');
       user.loading = false;
     });
+
+    user.sendFile = function() {
+      Upload.save({
+        picture: user.upload,
+        name: netId
+      }).$promise.then(function(data) {
+        // console.log(data);
+      });
+
+    };
   });
