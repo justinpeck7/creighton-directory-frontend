5f7626b5c992ad245ca5c6995ae3b9609f7c28ec
Justin Peck <jmpeck@up.com>
2015-12-03 12:45:45 -0600

    add edit profile functionality

--- user/user.js
+++ user/user.js
@@ -1,7 +1,7 @@
 /*This is our 'profile' page which shows more detailed information about each user*/
 angular.module('creightonDir.user', [
-    'ui.router'
-  ])
+  'ui.router'
+])
   .config(function($stateProvider) {
     $stateProvider.state('user', {
       url: '/user/:netId',
@@ -13,7 +13,7 @@ angular.module('creightonDir.user', [
       }
     });
   })
-  .controller('UserCtrl', function HomeController($rootScope, $stateParams, $resource, store, jwtHelper) {
+  .controller('UserCtrl', function HomeController($rootScope, $stateParams, $resource, store, jwtHelper, $uibModal) {
     $rootScope.showNavBar = true;
     var user = this,
       FindOne = $resource('http://localhost:3001/user/auth/findOne'),
@@ -22,21 +22,7 @@ angular.module('creightonDir.user', [
       netId = jwt.netId;
     user.loading = true;
 
-       function formDataObject (data) {
-            var fd = new FormData();
-            angular.forEach(data, function(value, key) {
-                fd.append(key, value);
-            });
-            return fd;
-        }
 
-    var Upload = $resource('http://localhost:3001/upload/auth/picture', {}, {
-      save: {
-        method: 'POST',
-        transformRequest: formDataObject,
-        headers: {'Content-Type':undefined, enctype:'multipart/form-data'}
-      }
-    })
 
     /*Find a single user and return results*/
     FindOne.get({
@@ -44,16 +30,57 @@ angular.module('creightonDir.user', [
     }).$promise.then(function(data) {
       user.info = data.user;
       user.info.groups = user.info.groups.join(', ');
+      user.showEditButton = user.info.netId === netId;
       user.loading = false;
     });
 
-    user.sendFile = function() {
+
+
+    user.showEdit = function() {
+      $uibModal.open({
+        animation: false,
+        templateUrl: 'user/editprofile.html',
+        controller: 'ModalCtrl',
+        controllerAs: 'modal',
+        size: 'lg'
+      });
+    };
+  })
+  .controller('ModalCtrl', function($scope, $resource, store, jwtHelper) {
+    var modal = this,
+      token = store.get('jwt'),
+      jwt = jwtHelper.decodeToken(token),
+      netId = jwt.netId;
+    modal.user = {};
+
+    function formDataObject(data) {
+      var fd = new FormData();
+      angular.forEach(data, function(value, key) {
+        fd.append(key, value);
+      });
+      return fd;
+    }
+
+    var Upload = $resource('http://localhost:3001/upload/auth/profile', {}, {
+      save: {
+        method: 'POST',
+        transformRequest: formDataObject,
+        headers: {
+          'Content-Type': undefined,
+          enctype: 'multipart/form-data'
+        }
+      }
+    });
+
+    modal.sendFile = function() {
       Upload.save({
-        picture: user.upload,
-        name: netId
+        picture: modal.upload,
+        netId: netId,
+        userData: JSON.stringify(modal.user)
       }).$promise.then(function(data) {
         // console.log(data);
+        $scope.$close();
       });
-
     };
+
   });
